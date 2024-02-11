8d1be2da2e08d1105ff59fb23472110fd3915f77
mrudulks <devmrudulks@gmail.com>
2024-02-12 00:05:47 +0530

    ui changes

--- pages/cart.vue
+++ pages/cart.vue
@@ -2,7 +2,7 @@
   <div class="xl:max-w-7xl lg:max-w-4xl mx-auto pt-12 pb-6 px-4">
     <h1 class="text-head text-3xl font-bold mb-4">CART</h1>
     <!-- Cart steps -->
-    <div class="grid grid-cols-3">
+    <div class="md:grid hidden grid-cols-3">
       <div
         v-for="(tab, idx) in checkoutTabs"
         :key="idx"
@@ -15,15 +15,31 @@
         </div>
       </div>
     </div>
+
+    <div class="md:hidden flex">
+      <template v-for="(tab, idx) in checkoutTabs">
+        <div
+          v-if="step == tab.value"
+          :key="idx"
+          class="text-lg text-head flex flex-1 gap-2 font-medium border-b-[2px] border-head py-5 cursor-pointer"
+        >
+          0{{ idx + 1 }}
+          <div @click="selectedTab = tab.value">
+            <h6>{{ tab.title }}</h6>
+            <p class="text-second text-sm font-normal">{{ tab.description }}</p>
+          </div>
+        </div>
+      </template>
+    </div>
     <!-- Cart steps -->
 
     <div class="py-10">
       <!-- Shopping bags -->
-      <Cart v-if="step == 'checkout'"/>
+      <Cart v-if="step == 'checkout'" />
       <!-- Shopping bags -->
-      <Address v-if="step == 'address'"/>
+      <Address v-if="step == 'address'" />
       <!-- Checkout form -->
-     
+
       <!--  -->
 
       <!-- Confirmation -->
@@ -115,15 +131,15 @@
 </template>
 
 <script>
-import Address from '~/components/Cart/Address.vue';
-import Cart from "~/components/Cart/index.vue"
+import Address from "~/components/Cart/Address.vue";
+import Cart from "~/components/Cart/index.vue";
 export default {
   name: "CartPage",
   layout: "main",
   components: {
     CheckBold: () => import("vue-material-design-icons/CheckBold.vue"),
     Cart,
-    Address
+    Address,
   },
   data() {
     return {
@@ -133,7 +149,7 @@ export default {
           title: "SHOPPING BAG",
           description: "Manage Your Items List",
           value: "checkout",
-          component:"Checkout"
+          component: "Checkout",
         },
         {
           title: "SHIPPING AND CHECKOUT",
@@ -152,13 +168,13 @@ export default {
       step: "checkout",
     };
   },
-  watch:{
-    '$route':{
-      handler(v){
+  watch: {
+    $route: {
+      handler(v) {
         const query = v.query;
-        if(query.step) this.step = v.query
-      }
-    }
+        if (query.step) this.step = v.query;
+      },
+    },
   },
   methods: {
     proceedCheckout() {
@@ -169,7 +185,7 @@ export default {
   },
   async mounted() {
     const query = this.$route.query;
-    if(query.step) this.step = query.step
+    if (query.step) this.step = query.step;
   },
 };
 </script>
