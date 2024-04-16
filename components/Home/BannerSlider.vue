023ade3c85dd488f818e4487ee0a007bf0588b7b
Vishnu Prakash <vishnuprakash@xeoscript.com>
2024-04-17 04:29:33 +0530

    fixes on banner

--- components/Home/BannerSlider.vue
+++ components/Home/BannerSlider.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home-banner relative">
     <div class="banner-images h-full">
-      <img src="~/assets/images/banner.png" class="h-full md:object-none object-cover" alt="" srcset="" />
+      <img :src="selectedBanner.image" class="h-full md:object-none object-cover" alt="" srcset=""/>
     </div>
     <!-- Banner text -->
     <div
       class="absolute text-head top-[50%] left-[20%] transform -translate-y-[50%]"
     >
-      <h1 class="text-5xl md:text-7xl capitalize">
+      <h1 class="text-5xl md:text-7xl capitalize" v-html="selectedBanner.title">
         {{ selectedBanner.title }}
       </h1>
-      <p class="text-base">{{ selectedBanner.description }}</p>
-      <button @click="onClickDiscover" class="text-lg font-medium border-b border-head pb-1 pt-4">
-        Discover Now
+      <p class="text-base" v-html="selectedBanner.short_description">{{ selectedBanner.short_description }}</p>
+      <button @click="onClickDiscover(selectedBanner.link)" class="text-lg font-medium border-b border-head pb-1 pt-4">
+        {{ selectedBanner.cta_text }}
       </button>
     </div>
     <div class="absolute bottom-10 left-[20%] flex gap-10">
@@ -26,13 +26,13 @@
       </button>
     </div>
     <div class="sm:block hidden absolute right-16 top-[50%] transform -translate-y-[50%]">
-        <ul class="flex flex-col gap-4 md:gap-6 items-center">
-            <li><img src="~/assets/images/icons/facebook.svg" alt="" srcset=""></li>
-            <li><img src="~/assets/images/icons/twitter.svg" alt="" srcset=""></li>
-            <li><img src="~/assets/images/icons/instagram.svg" alt="" srcset=""></li>
-            <li><img src="~/assets/images/icons/pinterest.svg" alt="" srcset=""></li>
-            <li><span class="text-second text-sm font-medium" style="writing-mode: tb-rl;">FOLLOW US</span></li>
-        </ul>
+      <ul class="flex flex-col gap-4 md:gap-6 items-center">
+        <li><img src="~/assets/images/icons/facebook.svg" alt="" srcset=""></li>
+        <li><img src="~/assets/images/icons/twitter.svg" alt="" srcset=""></li>
+        <li><img src="~/assets/images/icons/instagram.svg" alt="" srcset=""></li>
+        <li><img src="~/assets/images/icons/pinterest.svg" alt="" srcset=""></li>
+        <li><span class="text-second text-sm font-medium" style="writing-mode: tb-rl;">FOLLOW US</span></li>
+      </ul>
     </div>
   </div>
 </template>
@@ -43,20 +43,7 @@ export default {
   data() {
     return {
       imgIndex: 1,
-      banners: [
-        {
-          title: "The Classics",
-          description: "An exclusive selection of this season's trends.",
-        },
-        {
-          title: "fresh Threads",
-          description: "An exclusive selection of this season's trends.",
-        },
-        {
-          title: "Trend Fashion",
-          description: "An exclusive selection of this season's trends.",
-        },
-      ],
+      banners: [],
     };
   },
   watch: {
@@ -66,21 +53,41 @@ export default {
   },
   computed: {
     selectedBanner() {
-      return this.banners[this.imgIndex - 1];
+      if (this.banners.length !== 0) {
+        return this.banners[this.imgIndex - 1];
+      } else {
+        return {}
+      }
     },
   },
   methods: {
+    async getBanners() {
+      try {
+        const {data} = await this.$api.get("/cms/customer/hero-section/")
+        this.banners = data.results
+        console.log('Banners:', this.banners)
+      } catch (error) {
+        const errorText = error.response.status + ' ' + error.response.statusText
+        this.$alert.show({
+          isError: true,
+          title: error.response.status,
+          description: errorText,
+        });
+      }
+    },
     bannerSlider() {
       setTimeout(() => {
-        if (this.imgIndex == 3) this.imgIndex = 1;
+        const imageIndex = this.banners.length
+        if (this.imgIndex === imageIndex) this.imgIndex = 1;
         else this.imgIndex++;
       }, 5000);
     },
-    onClickDiscover(){
-      this.$router.push("/shop")
+    onClickDiscover(link) {
+      window.location.href = link;
     }
   },
   mounted() {
+    this.getBanners()
     this.bannerSlider();
   },
 };
