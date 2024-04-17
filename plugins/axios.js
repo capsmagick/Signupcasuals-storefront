1d93363efcf48edcf7d2db454cc341ca34d73702
Rahul M U <rmuwork51@gmail.com>
2024-04-17 05:30:19 +0530

    New Update on axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,60 +4,61 @@ import Cookies from "js-cookie";
 
 let csrfToken;
 
-export default function ({ store }, inject) {
-  const axiosAPI = axios.create({
-    xsrfHeaderName: "X-CSRFToken",
-    xsrfCookieName: "csrftoken",
-    withCredentials: true,
-    baseURL: "https://manage.signupcasuals.com:8443/api",
-    headers:{
-      Accept:"application/json"
-    }
-  });
-
-  const setTokenToHeaders = function (request) {
+const setTokenToHeaders = function (request) {
     if (csrfToken) {
-      request.headers["X-CSRFToken"] = csrfToken;
+        request.headers["X-CSRFToken"] = csrfToken;
     }
-  };
+};
 
-  const getTokenFromResponse = function () {
+const getTokenFromResponse = function () {
     csrfToken = Cookies.get("csrftoken");
-  };
+};
 
-  // Adding request interceptor to attach CSRF token to outgoing requests
-  axiosAPI.interceptors.request.use(
-    (config) => {
-      const method = config.method;
-      if (method !== "get" || method !== "delete") {
-        if (
-          typeof FormData !== "undefined" &&
-          config.data instanceof FormData
-        ) {
-          config.headers["Content-Type"] = "multipart/form-data";
+export default function ({store}, inject) {
+    const axiosAPI = axios.create({
+        xsrfHeaderName: "X-CSRFToken",
+        xsrfCookieName: "csrftoken",
+        withCredentials: true,
+        baseURL: "https://manage.signupcasuals.com:8443/api",
+        // baseURL: "http://localhost:8000/api",
+        headers: {
+            Accept: "application/json"
         }
-      }
-      setTokenToHeaders(config);
+    });
 
-      // if (store.state.access) {
-      //   config.headers["Authorization"] = `Bearer ${store.state.access}`;
-      // }
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
+    // Adding request interceptor to attach CSRF token to outgoing requests
+    axiosAPI.interceptors.request.use(
+        (config) => {
+            const method = config.method;
+            if (method !== "get" || method !== "delete") {
+                if (
+                    typeof FormData !== "undefined" &&
+                    config.data instanceof FormData
+                ) {
+                    config.headers["Content-Type"] = "multipart/form-data";
+                }
+            }
+            setTokenToHeaders(config);
 
-  axiosAPI.interceptors.response.use(
-    function (response) {
-      getTokenFromResponse();
-      return response;
-    },
-    function (error) {
-      return Promise.reject(error);
-    }
-  );
+            // if (store.state.access) {
+            //   config.headers["Authorization"] = `Bearer ${store.state.access}`;
+            // }
+            return config;
+        },
+        (error) => Promise.reject(error)
+    );
 
-  inject("api", axiosAPI);
+    axiosAPI.interceptors.response.use(
+        function (response) {
+            getTokenFromResponse();
+            return response;
+        },
+        function (error) {
+            return Promise.reject(error);
+        }
+    );
+
+    inject("api", axiosAPI);
 }
 
 // export default axiosAPI;
